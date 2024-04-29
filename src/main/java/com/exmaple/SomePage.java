ab1f4610c5677a19fb45c1c6f2c319cfa487512e
Abhishek Vishnoi <abvishno@redhat.com>
2024-04-29 10:50:09 +0530

    First Commit for a quarkus application with AD integration .

---
+++ src/main/java/com/exmaple/SomePage.java
@@ -0,0 +1,40 @@
+package com.exmaple;
+
+import io.quarkus.qute.Template;
+import io.quarkus.qute.TemplateInstance;
+
+import io.quarkus.security.Authenticated;
+import io.quarkus.security.identity.SecurityIdentity;
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
+
+import static java.util.Objects.requireNonNull;
+
+@Path("/some-page")
+//@Authenticated
+@RolesAllowed("Admin")
+public class SomePage {
+
+    private final Template page;
+
+    @Inject
+    SecurityIdentity identity;
+
+    public SomePage(Template page) {
+        this.page = requireNonNull(page, "page is required");
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public TemplateInstance get(@QueryParam("name") String name) {
+        return page
+                .data("name", name)
+                .data("identity", identity);
+    }
+
+}
